1ad7729cb3f4d4113fb7001f7112d65e81dcf028
wangilindalestarii <wangilindalestari23@gmail.com>
2021-04-07 12:49:27 +0800

    OperatorBinaryAritmatika

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,22 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variabel
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+
+    String nama = "Wangi Linda Lestari";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9 + 3);
+        System.out.println(9 - 3);
+        System.out.println(9 * 3);
+        System.out.println(9 / 3);
+        System.out.println(9 % 3);
+        System.out.println(9 % 2);
+    }
+}
