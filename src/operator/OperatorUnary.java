bc78becdd298118bc04ded3ea23c7fee6bb92b6d
wangilindalestarii <wangilindalestari23@gmail.com>
2021-04-07 12:54:31 +0800

    OperatorUnary.java

---
+++ src/operator/OperatorUnary.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class OperatorUnary {
+    public static void main(String[] args) {
+        int i = 0;
+        i++;
+        System.out.println(i);
+        i++;
+        System.out.println(i);
+        i=1+1;
+        System.out.println(i);
+    }
+}
